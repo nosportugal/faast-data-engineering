4b7ad436f4081318ff711b4c6704a70fd5fbbdea
Fernando Cordeiro <fernando.cordeiro@parceiros.nos.pt>
2023-03-01 13:27:11 +0000

    🐛 FIX: Path not found

--- 3_data_warehouse/code/extras/web_to_gcs.py
+++ 3_data_warehouse/code/extras/web_to_gcs.py
@@ -3,19 +3,24 @@ import os
 import requests
 import pandas as pd
 import pyarrow
+
+from pathlib import Path
 from google.cloud import storage
 
 """
 Pre-reqs: 
-1. `pip install requests pandas pyarrow google-cloud-storage` in your virtual env or DockerFile
-2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
+1. `pip install requests pandas pyarrow google-cloud-storage` in your virtual env
+  or DockerFile
+2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key 
+  OR run "gcloud auth application-default login" in your terminal to authenticate
 3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
 """
 
-# services = ['fhv','green','yellow']
-init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
-# switch out the bucketname
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc-data-lake-bucketname")
+init_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/"
+
+# Add GCP project and bucket name
+PROJECT_ID = os.environ.get("GCP_PROJECT", "faast-data-engineering")
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "gcs-faast-data-engineering")
 
 
 def upload_to_gcs(bucket, object_name, local_file):
@@ -27,7 +32,7 @@ def upload_to_gcs(bucket, object_name, local_file):
     # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
     # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
 
-    client = storage.Client()
+    client = storage.Client(project=PROJECT_ID)
     bucket = client.bucket(bucket)
     blob = bucket.blob(object_name)
     blob.upload_from_filename(local_file)
@@ -35,34 +40,36 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 def web_to_gcs(year, service):
     for i in range(12):
-        
         # sets the month part of the file_name string
-        month = '0'+str(i+1)
+        month = "0" + str(i + 1)
         month = month[-2:]
 
-        # csv file_name 
-        file_name = f"{service}/{service}_tripdata_{year}-{month}.csv.gz"
+        # csv file_name
+        output_dir = Path(f"{service}/")
+        output_dir.mkdir(parents=True, exist_ok=True)
+        file_name = output_dir / f"{service}_tripdata_{year}-{month}.csv.gz"
 
         # download it using requests via a pandas df
-        request_url = init_url + file_name
+        request_url = init_url + file_name.as_posix()
         response = requests.get(request_url)
         fp = io.BytesIO(response.content)
-        df = pd.read_csv(fp, compression='gzip')
+        df = pd.read_csv(fp, compression="gzip")
         df.to_csv(file_name)
         print(f"Local: {file_name}")
 
         # read it back into a parquet file
         df = pd.read_csv(file_name)
-        file_name = file_name.replace('.csv.gz', '.parquet')
-        df.to_parquet(file_name, engine='pyarrow')
+        file_name = file_name.as_posix().replace(".csv.gz", ".parquet")
+        df.to_parquet(file_name, engine="pyarrow")
         print(f"Parquet: {file_name}")
 
-        # upload it to gcs 
+        # upload it to gcs
         upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
         print(f"GCS: {service}/{file_name}")
 
 
-web_to_gcs('2019', 'green')
-web_to_gcs('2020', 'green')
-# web_to_gcs('2019', 'yellow')
-# web_to_gcs('2020', 'yellow')
+if __name__ == "__main__":
+    web_to_gcs("2019", "green")
+    web_to_gcs("2020", "green")
+    web_to_gcs("2019", "yellow")
+    web_to_gcs("2020", "yellow")
