143b73e7195aa71ed9407a0843eb1f1301772d67
Fernando Cordeiro <fernando.cordeiro@parceiros.nos.pt>
2023-03-13 17:19:17 +0000

    👕 LINT: Format indentation

--- 6_stream_processing/consumer.py
+++ 6_stream_processing/consumer.py
@@ -4,16 +4,17 @@ from time import sleep
 
 consumer = KafkaConsumer(
     'demo_1',
-     bootstrap_servers=['localhost:9092'],
-     auto_offset_reset='earliest',
-     enable_auto_commit=True,
-     group_id='consumer.group.id.demo.2',
-     value_deserializer=lambda x: loads(x.decode('utf-8')))
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='consumer.group.id.demo.2',
+    value_deserializer=lambda x: loads(x.decode('utf-8'))
+)
 
 
 while(True):
- print("inside while")
- for message in consumer:
-  message = message.value
-  print(message)
- sleep(1)
+    print("inside while")
+    for message in consumer:
+        message = message.value
+        print(message)
+    sleep(1)
